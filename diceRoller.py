b956d41c82cf5559b93756e06a7fe97b8f082c53
Abigail Thompson <athompson6984@sohamcollege.org.uk>
2014-05-22 22:44:24 +0100

    Update diceRoller.py

--- diceRoller.py
+++ diceRoller.py
@@ -5,6 +5,7 @@ import random,time
 #This imports random to make the number rolled a random number (between 1 and 6 inclusive)
 #This imports time to add a pause between generating the random number and printing it to the user.
 
+roll = 0
 s1 = "- - - - -\n|       |\n|   O   |\n|       |\n- - - - -\n"
 s2 = "- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n"
 s3 = "- - - - -\n| O     |\n|   O   |\n|     O |\n- - - - -\n"
@@ -12,6 +13,17 @@ s4 = "- - - - -\n| O   O |\n|       |\n| O   O |\n- - - - -\n"
 s5 = "- - - - -\n| O   O |\n|   O   |\n| O   O |\n- - - - -\n"
 s6 = "- - - - -\n| O   O |\n| O   O |\n| O   O |\n- - - - -\n"
 #All of these variables, when printed, display the face of the dice that the random number is.
+def request():
+    confirm = 0
+    while confirm != "Y" or "Y6" or "YT"::
+        confirm = input("Enter 'Y' to roll the dice\n'Y6' to roll the dice until it rolls a 6\nor 'YT' to roll the dice until it rolls the same number twice in a row >>> ")
+        confirm = confirm.upper()
+        if confirm == "Y":
+            run()
+        if confirm == "Y6":
+            run6()
+        if confirm == "YT":
+            runT()
 
 def rollnumber():
     #This function generates a random number between 1 and 6 inclusive, turning it into a variable.
@@ -35,9 +47,18 @@ def show_dice(roll): #This uses the random number to print off the right face.
 
 #This part of the program runs the functions in the correct order
 #It is therefore the main part of the program.
-rollnumber()
-time.sleep(1)
-show_dice(roll)
+def run():
+    rollnumber()
+    time.sleep(1)
+    show_dice(roll)
+
+def run6(): #This function rolls the dice until it rolls a 6, and then stops.
+    while roll != 6:
+        run()
+        if roll == 6:
+            break
+
+request()
 
 ###Some of the errors that I found were:
     # when importing modules, it put a ; between the requested imports rather than a ,
@@ -45,3 +66,4 @@ show_dice(roll)
     # import was misspelt
     # They used the wrong use of the random generator. I changed it to random.randint(1,6), which made it generate a random integer between 1 and 6 inclusive.
     # the show_dice() function did not request a parameter
+
